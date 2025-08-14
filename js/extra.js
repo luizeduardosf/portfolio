7a42a1af22106f711d28e5a59f439b5f665dc9fb
Luiz Eduardo <30324149+luizeduardosf@users.noreply.github.com>
2025-08-14 11:22:16 -0300

    Add files via upload

---
+++ js/extra.js
@@ -0,0 +1,111 @@
+function webdesign() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Saiba mais...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Fechar";
+    moreText.style.display = "inline";
+  }
+}
+
+function design() {
+  var dots5 = document.getElementById("dots5");
+  var moreText5 = document.getElementById("more5");
+  var btnText5 = document.getElementById("myBtn5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    btnText5.innerHTML = "Saiba mais...";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none";
+    btnText5.innerHTML = "Fechar";
+    moreText5.style.display = "inline";
+  }
+}
+
+function ads() {
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "Saiba mais...";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "Fechar";
+    moreText3.style.display = "inline";
+  }
+}
+
+function branding() {
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText4 = document.getElementById("myBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Saiba mais...";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Fechar";
+    moreText4.style.display = "inline";
+  }
+}
+
+function videomkt() {
+  var dots7 = document.getElementById("dots7");
+  var moreText7 = document.getElementById("more7");
+  var btnText7 = document.getElementById("myBtn7");
+
+  if (dots7.style.display === "none") {
+    dots7.style.display = "inline";
+    btnText7.innerHTML = "Saiba mais...";
+    moreText7.style.display = "none";
+  } else {
+    dots7.style.display = "none";
+    btnText7.innerHTML = "Fechar";
+    moreText7.style.display = "inline";
+  }
+}
+
+function copywriting() {
+  var dots6 = document.getElementById("dots6");
+  var moreText6 = document.getElementById("more6");
+  var btnText6 = document.getElementById("myBtn6");
+
+  if (dots6.style.display === "none") {
+    dots6.style.display = "inline";
+    btnText6.innerHTML = "Saiba mais...";
+    moreText6.style.display = "none";
+  } else {
+    dots6.style.display = "none";
+    btnText6.innerHTML = "Fechar";
+    moreText6.style.display = "inline";
+  }
+}
+
+function socialmedia() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Saiba mais...";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Fechar";
+    moreText2.style.display = "inline";
+  }
+}
